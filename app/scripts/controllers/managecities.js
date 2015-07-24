fb18c82f9b845cf46d64ae98d09b65487511a240
nilesh.tochia <nilesh.tochia@gmail.com>
2015-07-24 13:11:36 +0100

    Adds controllers and service

---
+++ app/scripts/controllers/managecities.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name weatherAppApp.controller:ManagecitiesCtrl
+ * @description
+ * # ManagecitiesCtrl
+ * Controller of the weatherAppApp
+ */
+angular.module('weatherAppApp')
+  .controller('ManagecitiesCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/services/weatherapi.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name weatherAppApp.weatherApi
+ * @description
+ * # weatherApi
+ * Service in the weatherAppApp.
+ */
+angular.module('weatherAppApp')
+  .service('weatherApi', function () {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
