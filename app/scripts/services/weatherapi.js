e3acc0b67562e6b0d888fa052e16881b11bab1ab
nilesh.tochia <nilesh.tochia@gmail.com>
2015-07-24 13:45:07 +0100

    Adds get current weather to weather api service

--- app/scripts/services/weatherapi.js
+++ app/scripts/services/weatherapi.js
@@ -8,6 +8,20 @@
  * Service in the weatherAppApp.
  */
 angular.module('weatherAppApp')
-  .service('weatherApi', function () {
-    // AngularJS will instantiate a singleton by calling "new" on this function
+  .service('weatherApi', function ($resource) {
+    var CurrentWeatherResource = $resource(
+       'http://api.openweathermap.org/data/2.5/weather',
+       {},
+       {
+           get: {
+               method: 'GET'
+           }
+       }
+   );
+
+   this.getCurrentWeather = function (city, callback){
+       var cityData = CurrentWeatherResource.get({q: city}, function (){
+         callback(cityData);
+       });
+   };
   });
