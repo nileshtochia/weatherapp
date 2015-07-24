2568c341ddef307d3b130cee3692085470bb4379
nilesh.tochia <nilesh.tochia@gmail.com>
2015-07-24 12:59:53 +0100

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name weatherAppApp
+ * @description
+ * # weatherAppApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('weatherAppApp', [
+    'ngAnimate',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
